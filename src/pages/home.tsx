35510aa446f6be91b09c87b5849375ca6427b543
fyoussef <fillipyuou@gmail.com>
2022-11-14 13:00:07 -0300

    Alterando altura do menu

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -18,7 +18,7 @@ export default function Home() {
     return (
         <Flex
             bg='gray.900'
-            h='100vh'
+            h='full'
             color='white'
         >
             <Head>
